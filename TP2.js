d5c50a53ab67dd98dddd13726c87258a3e0c0a8d
Corbeil <e1455714@cmaisonneuve.qc.ca>
2017-03-28 17:12:35 -0400

    création du projet de base

--- TP2.js
+++ TP2.js
@@ -20,54 +20,6 @@ MongoClient.connect('mongodb://127.0.0.1:27017/carnet', (err, database) => {
 
 
 
-
-//page de base
-app.get('/',  (req, res) => {
-   console.log('la route route get / = ' + req.url)
-   //récupère la bdd
-    var cursor = db.collection('carnet').find().toArray(function(err, resultat){
-       if (err) return console.log(err)
-    // affiche le contenu de la BD
-    res.render('index.ejs', {carnet: resultat})
-
-    }) 
-})
-
-
-//lorsqu'on ajoute une adresse
-app.post('/ajout',  (req, res) => {
-  db.collection('carnet').save({'nom':req.body[0].value,
-  "telephone":req.body[2].value,"prenom":req.body[1].value}, (err, result) => {
-      if (err) return console.log(err)
-      console.log('sauvegarder dans la BD')
-      //console.log(result[3].value);
-      console.log(result.ops[0]._id);
-      res.send(result.ops[0]._id);
-    })
-})
-
-//lorsqu'on delete une adresse
-app.post('/delete',  function(req, res){
-  //supprime l'adresse sélectionnée
-db.collection('carnet').remove ({_id: ObjectId(req.body["_id"])}, (err, result) => {
-  if (err) {return console.log(err)}
-    //retourne à la page de base
-      res.end()
-    })
-})
-
-
-//lorsqu'on confirme l'update d'une adresse
-app.post('/update',  function(req, res){
-  //collecte les valeurs entrées pour changer l'adresse sélectionnée
-db.collection('carnet').update ({_id: ObjectId(req.body["_id"])},{$set:{'nom':req.body["nom"],
-  "telephone":req.body["telephone"],"prenom":req.body["prenom"]}}, (err, result) => {
-  if (err) {return console.log(err)}
-      console.log("reussi !");
-      //retourne à la page de base
-      //res.redirect('/') 
-      res.end()
-  })
 })
 
 
